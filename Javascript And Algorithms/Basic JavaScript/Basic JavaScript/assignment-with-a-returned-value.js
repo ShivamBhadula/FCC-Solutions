ae8356939d3db533bce97553f40f419927c9b5fc
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-08-26 23:26:26 +0530

    Add files via upload

---
+++ Javascript And Algorithms/Basic JavaScript/Basic JavaScript/assignment-with-a-returned-value.js
@@ -0,0 +1,7 @@
+// Setup
+var processed = processArg(7);
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+// Only change code below this line
---
+++ Javascript And Algorithms/Basic JavaScript/Basic JavaScript/increment-a-number-with-javascript.js
@@ -0,0 +1,3 @@
+var myVar = 87;
+// Only change code below this line
+myVar++;
