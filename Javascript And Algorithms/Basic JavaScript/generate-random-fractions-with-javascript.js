b3535a0003730522c997d60252b8a5007fc95e00
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-08-27 23:43:19 +0530

    Add files via upload

---
+++ Javascript And Algorithms/Basic JavaScript/generate-random-fractions-with-javascript.js
@@ -0,0 +1,8 @@
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random(0,1);
+
+  // Only change code above this line
+}
---
+++ Javascript And Algorithms/Basic JavaScript/use-recursion-to-create-a-range-of-numbers.js
@@ -0,0 +1,10 @@
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum-startNum===0)
+  return [startNum];
+  else
+  {
+    var arr=rangeOfNumbers(startNum,endNum-1);
+    arr.push(endNum);
+    return arr;
+  }
+};
