b663be639197846361dcaf3d42138f25d84f77aa
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-09-09 22:54:04 +0530

    Add files via upload

---
+++ Javascript And Algorithms/ES6/write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+const bicycle = {
+  
+  setGear(newGear) {
+    this.gear= newGear;
+    return this.gear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
